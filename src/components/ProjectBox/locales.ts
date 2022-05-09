28e138545a9b9858364c0666155d42f191f151d7
mohamedelserafy <elserafy02@gmail.com>
2022-05-09 19:04:13 +0200

    loremipsum

---
+++ src/components/ProjectBox/locales.ts
@@ -0,0 +1,21 @@
+import { TranslationNameSpace } from "../../interfaces/translationNameSpace";
+
+const projectTranslations: TranslationNameSpace = {
+  ar: {
+    Loremipsum:
+      "أكسير سيتاشن يللأمكو لابورأس نيسي يت أليكيوب أكس أيا كوممودو كونسيكيوات . ديواس  أيوتي أريري دولار إن ريبريهينديرأيت فوليوبتاتي فيلايت أيسسي كايلليوم دولار أيو فيجايت  نيولا باراياتيور. أيكسسيبتيور ساينت أوككايكات كيوبايداتات",
+    virtualLibiray: "مكتبه افتراضيه",
+  },
+  en: {
+    Loremipsum:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedo eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+    virtualLibiray: "Virtual Libirary",
+  },
+  fr: {
+    Loremipsum:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedo eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+    virtualLibiray: "Librairie Virtuelle",
+  },
+};
+
+export default projectTranslations;
