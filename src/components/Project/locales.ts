c37a6589971cad4354e8c2a8bf300c188ac40c96
mohamedelserafy <elserafy02@gmail.com>
2022-05-09 19:09:44 +0200

    Project locales

---
+++ src/components/Project/locales.ts
@@ -0,0 +1,9 @@
+import { TranslationNameSpace } from "./../../interfaces/translationNameSpace";
+
+const projectTranslations: TranslationNameSpace = {
+  ar: { MyProjects: "مشاريع" },
+  en: { MyProjects: "My Projects" },
+  fr: { MyProjects: "Mes Projects" },
+};
+
+export default projectTranslations;
